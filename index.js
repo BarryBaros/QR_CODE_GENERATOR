3d40a502d12c628df26499bd848385d71e466531
Barasa <timbaros87@gmail.com>
2024-08-10 13:44:33 +0300

    Get documents from HTML

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const user_name = document.querySelector('.user_name');
 const user_email = document.querySelector('.user_email');
 const user_phone = document.querySelector('.user_phne');
 
+const generateButton = document.querySelector('.generate_qr_code_button');
+
+const loader = document.querySelector('.loader');
+
+const qrImage = document.querySelector('.qr_image');
+
