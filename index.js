ad9802649833ac4883cec4dae4a020905c48243f
Barasa <timbaros87@gmail.com>
2024-08-10 14:25:13 +0300

    JavaScript complete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const user_name = document.querySelector('.user_name');
 const user_email = document.querySelector('.user_email');
-const user_phone = document.querySelector('.user_phne');
+const user_phone = document.querySelector('.user_phone');
 
 const generateButton = document.querySelector('.generate_qr_code_button');
 
@@ -8,3 +8,27 @@ const loader = document.querySelector('.loader');
 
 const qrImage = document.querySelector('.qr_image');
 
+generateButton.onclick = async () => {
+    qrImage.src = '';
+    let userNameValue = user_name.value
+    let userEmailValue = user_email.value
+    let userPhoneValue = user_phone.value
+
+    let allValues = `${userNameValue} ${userEmailValue} ${userPhoneValue}`;
+
+    loader.style.display = 'block';
+    
+    if(userEmailValue.length == 0 || userEmailValue.length == 0 || userPhoneValue.length == 0){
+        alert('Enter valid credentials!');
+        loader.style.display = 'none';
+    }else{
+        
+        let imageSource = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${allValues}`;
+        let data = await fetch(imageSource);
+        let response = await data.blob();
+        
+        let url = URL.createObjectURL(response);
+        qrImage.src = url;
+        loader.style.display = 'none';
+    }
+}
